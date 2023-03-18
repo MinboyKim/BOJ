e8bf94b39aa123a252ecb2316185e45221be0be8
MinboyKim <kdm3801@gmail.com>
2023-03-18 19:57:44 +0900

    Minboy 3/18 BRONZE3_2444

---
+++ 23_March/18/BRONZE3_2444_Minboy.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> N;
+  for (int i = 1; i <= N; i++) {
+    for (int j = 0; j < N - i; j++) cout << " ";
+    for (int k = 0; k < 2 * i - 1; k++) cout << "*";
+    cout << "\n";
+  }
+  for (int i = 1; i < N; i++) {
+    for (int j = 0; j < i; j++) cout << " ";
+    for (int k = 0; k < 2 * N - (2 * i + 1); k++) cout << "*";
+    cout << "\n";
+  }
+
+  return 0;
+}
