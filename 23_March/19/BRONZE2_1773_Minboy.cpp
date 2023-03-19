b1df4da24e31851c575c99c3185e92935166998d
MinboyKim <kdm3801@gmail.com>
2023-03-19 16:57:00 +0900

    김동민 3/19 BRONZE2_1773

---
+++ 23_March/19/BRONZE2_1773_Minboy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int N, C, ans;
+int Arr[111];
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> N >> C;
+  for (int i = 0; i < N; i++) cin >> Arr[i];
+  for (int i = 1; i <= C; i++) {
+    for (int j = 0; j < N; j++) {
+      if (i % Arr[j] == 0) {
+        ans++;
+        break;
+      }
+    }
+  }
+
+  cout << ans << "\n";
+
+  return 0;
+}
