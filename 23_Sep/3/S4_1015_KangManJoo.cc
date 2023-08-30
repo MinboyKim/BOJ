c32b9afc188498b9404fddc307dbde828e94ba23
kangdaehoon <eogns47@konkuk.ac.kr>
2023-08-31 05:55:07 +0900

    1015 09/03

---
+++ 23_Sep/3/S4_1015_KangManJoo.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  pair<int, int> arr[1001];
+
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i].first;
+    arr[i].second = i;
+  }
+  sort(arr, arr + n);
+  
+  int answer[1001];
+
+  for(int i=0; i<n; i++){
+    answer[arr[i].second]=i;
+  }
+  for(int i=0; i<n; i++){
+    cout<<answer[i]<<" ";
+  }
+}
